af3721684ccb5b15c729bb4050bc33d1f59e4e13
infktd <25016104+infktd@users.noreply.github.com>
2026-02-27 23:15:47 -0600

    fix: add sha256 to cask for integrity verification

--- Casks/snipt.rb
+++ Casks/snipt.rb
@@ -2,6 +2,8 @@ cask "snipt" do
   version "0.1.0"
 
   arch arm: "arm64", intel: "amd64"
+  sha256 arm:   "4901a36455628f998f949ad8286dadf6be630db7b72371d10ccb4b231f8516a2",
+         intel: "170041b73972edd73b058f8b272f0d3703b78d89675f4e6c8ec8fefb3aa54a91"
 
   url "https://github.com/infktd/snipt/releases/download/v#{version}/snipt-macos-#{arch}.zip"
   name "snipt"
